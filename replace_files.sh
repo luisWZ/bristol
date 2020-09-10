f03db65643fac03e31129cf8b1b34707a9a4c48e
Luis Lasser <hola@luislasser.com>
2020-09-09 22:09:12 -0500

    Running script

--- replace_files.sh
+++ replace_files.sh
@@ -22,18 +22,20 @@ if [ $WEBSITE_FULL_ROUTE == $(pwd) ]
     echo "Moving files"
     for file in $files
       do
+        echo "Moving $file"
         mv $file $TEMP_DIR
     done
 
-    echo "Uncompress tar file"
-    tar -xzvf $COMPRESS_FILE
+    # echo "Uncompress tar file"
+    # tar -xzvf $COMPRESS_FILE
 
-    echo "Removing files"
+    # echo "Removing files"
     # [ -f .DS_Store ] && rm .DS_Store
     # [ -f CNAME ] && rm CNAME
     # rm -rf $TEMP_DIR $COMPRESS_FILE
 
-    echo "Files on production 🛫"
+    # echo "Files on production 🛫"
+    echo "Done  ¯\_(ツ)_/¯"
 
   else
     echo "You're pointing to an incorrect folder 💩"
