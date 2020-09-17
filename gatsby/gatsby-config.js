2643a5fccc3b858633883d5d406ca5d1c5c5be0f
Luis Lasser <hola@luislasser.com>
2020-09-17 18:51:40 -0500

    Configuring circleci

--- gatsby/gatsby-config.js
+++ gatsby/gatsby-config.js
@@ -91,6 +91,7 @@ module.exports = {
       options: {
         spaceId: process.env.CONTENTFUL_SPACE_ID,
         accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+        downloadLocal: true,
       },
     },
     {
