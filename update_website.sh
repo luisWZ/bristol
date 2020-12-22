d5c5daf47091e55ec1f7f23eeb5ca975b2012750
Luis Lasser <hola@luislasser.com>
2020-12-21 22:48:02 -0600

    Fixed error on diploma svg, change info on docker file

--- update_website.sh
+++ update_website.sh
@@ -7,7 +7,7 @@ cd $REMOTE_DIR
 if [ -f $COMPRESS_FILE ]
   then
 
-    echo "Creating $TEMP_DIR folder"
+    echo "Creating temp folder"
     [ -d $TEMP_DIR ] && rm -rf $TEMP_DIR
     mkdir $TEMP_DIR
 
@@ -19,7 +19,7 @@ if [ -f $COMPRESS_FILE ]
       -name $COMPRESS_FILE -prune  -o \
       -print)
 
-    echo "Moving old files to $TEMP_DIR folder"
+    echo "Moving old files to temp folder"
     for file in $files
       do
         mv $file $TEMP_DIR 2>/dev/null
@@ -28,7 +28,7 @@ if [ -f $COMPRESS_FILE ]
     echo "Uncompress tar file"
     tar -xzf $COMPRESS_FILE
 
-    echo "Removing files"
+    echo "Removing unnecessary files"
     rm -rf $TEMP_DIR $COMPRESS_FILE
 
     echo "Updated website 🛫"
