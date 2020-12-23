2cc89ea7ecef72e4a63d79a2715de3c19bb23f44
Luis Lasser <hola@luislasser.com>
2020-12-23 09:49:54 -0600

    Fix z-index Header

--- gatsby/gatsby-config.js
+++ gatsby/gatsby-config.js
@@ -67,20 +67,20 @@ module.exports = {
         once: false,
       },
     },
-    // {
-    //   resolve: `gatsby-plugin-manifest`,
-    //   options: {
-    //     name: title,
-    //     short_name: shortName,
-    //     lang,
-    //     start_url: `/`,
-    //     background_color: bristolBlue,
-    //     theme_color: bristolBlue,
-    //     display: `standalone`,
-    //     icon: `static/manifest-icon-bristol.png`,
-    //     cache_busting_mode: `none`,
-    //   },
-    // },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: title,
+        short_name: shortName,
+        lang,
+        start_url: `/`,
+        background_color: bristolBlue,
+        theme_color: bristolBlue,
+        display: `standalone`,
+        icon: `static/manifest-icon-bristol.png`,
+        cache_busting_mode: `none`,
+      },
+    },
     // {
     //   resolve: `gatsby-plugin-offline`,
     //   options: {
